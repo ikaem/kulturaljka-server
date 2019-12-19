e5c22b7e6b010742def3a333492c7102ac60ce0b
Karlo Marinovic <36795522+ikaem@users.noreply.github.com>
2019-12-19 20:28:50 +0100

    done with routing

--- server.js
+++ server.js
@@ -3,18 +3,18 @@ const database = require("./tempDatabase");
 const bcrypt = require("bcryptjs");
 const cors = require("cors");
 const knex = require("knex")({
-        // client: "pg",
-        // connection: {
-        //     host: '127.0.0.1',
-        //     user: "postgres",
-        //     password: "anVvPRpp",
-        //     database: 'kulturaljka'
-        // }
         client: "pg",
+        connection: {
+            host: '127.0.0.1',
+            user: "postgres",
+            password: "anVvPRpp",
+            database: 'kulturaljka'
+        }
+/*         client: "pg",
         connection: {
             connectionString: process.env.DATABASE_URL,
             ssl: true
-        }
+        } */
     })
 
     //     "nodemon": "^2.0.2"
@@ -60,179 +60,183 @@ app.use(cors());
 // -------------  // ENDPOINTS START // -------------- //
 // "/"GET - fetch all artists
 app.get("/", (req, res) => {
-    res.json("It works");
+    db("artists")
+    .select("*")
+    .returning("*")
+    .then(data => {
+        res.json(data);
+    })
+    .catch(console.log);
+
+    // 
+    // res.json("It works");
 })
 // "/login"POST - receive login data and return response
 app.post("/login", (req, res) => {
     const {email, password} = req.body;
-
-
-    console.log(req.body);
     if(email && password){
-        db("login").where(
-            {
-                email: email
-            }
-        )
+        db("login").where({email: email})
         .select("*")
-        //.then(console.log)
         .then(data => {
             if(data.length){
                 bcrypt.compare(password, data[0].hash, function(err, match){
                     if(err){
                         console.log("errror checking credentials: ", err);
-                        return res.status(400).json({
-                            data: err, 
-                            message: "error checking credentials"
-                        });
+                        return res.status(400).json({data: err, message: "error checking credentials"});
                     }
                     if(match){
-                        console.log({name: data[0].name, email: data[0].email})
-                        res.json({
-                            data: {name: data[0].name, email: data[0].email}, 
-                            message: "Login success"
-                        });
+                        res.json({data: {name: data[0].name, email: data[0].email}, message: "Login success"});
                     }
                     else{
                         console.log("incorrect credentials combination")
-                        res.json({
-                            data: {},
-                            message: "incorrect credentials combination"
-                        });
+                        res.json({data: {}, message: "incorrect credentials combination"});
                     }
                 })
             }
             else{
                 console.log("no such user")
-                res.json({
-                    data: {},
-                    message: "incorrect credentials combination"
-                });
+                res.json({data: {}, message: "incorrect credentials combination"});
             }
         })
         .catch(console.log)
     }
     else{
-        res.json({
-            data: {},
-            message: "Please submit all fields"
-        });
+        res.json({data: {}, message: "Please submit all fields"});
     }    
 })
-
 // "/register"POST - receive register data and return response
 app.post("/register", (req, res) => {
     const {name, email, password} = req.body;
-    
-
     if(name && email && password){
         console.log(req.body);
-        // check if same email already exists, bu in the eb
-
-        db("login").where(
-            {
-                email: email
-            }
-        )
+        db("login").where({email: email})
         .select("email")
         .then(data => {
             if(!data.length){
-                //userExists = false;
-
-
-                
-                // hash the password 
                 bcrypt.genSalt(10, (err, salt) => {
                     bcrypt.hash(password, salt, (err, hash) => {
-                    db("login").insert({
-                        name: name,
-                        email: email,
-                        hash: hash
-                    })
+                        db("login").insert({name: name,email: email, hash: hash})
                     .returning(["name", "email"])
                     .then(data => res.json(
-                        {
-                            data: {
-                                name: data[0].name, 
-                                email: data[0].email
-                            },
-                            message: "registration success"
-                        }
-                        ))
-                    // catch bi isto trebao vratiti do servera
+                        {data: {name: data[0].name,email: data[0].email},message: "registration success"}))
                     .catch(err => console.log("error", err))
                     })
                 })
             }
-            else{
-                // userExists = true;
-                res.json(
-                    {
-                        data: "",
-                        message: "email already in use"
-                    }
-                    )
-            }  
+            else{res.json({data: "",message: "email already in use"})}  
         })
-        .catch(err => console.log("Tu negdje", err));
+        .catch(err => console.log(err));
     }
-    else{
-        res.json({
-            data: "", 
-            message: "incomplete user data"
-        })
+    else{res.json({data: "", message: "incomplete user data"})
     }
 })
 
 // "/addartist"POST - receive new artist and return response
 app.post("/addartist", (req, res) => {
-    const {id, name, image, alt, genre, city, web, submittedBy, submittedOn, lastUpdatedOn, lastUpdatedBy} = req.body;
-    console.log("izvan true provjere", req.body);
-    if(id && name && image && alt && genre && city && web && submittedBy && submittedOn && lastUpdatedOn && lastUpdatedBy){
-        // check if we already have the same band
-        // jer prilikom registracije trebam pretraziti ima li vec takav neki...
-        console.log("unutar true provjere: ", req.body);
-
-        const sameArtist = artists.find((artist, index) => {
-            return artist.name === name
+const {name, image, alt, genre, city, web, submittedby, submittedon, lastupdatedon, lastupdatedby} = req.body;
+    if(name, image, alt, genre, city, web, submittedby, submittedon, lastupdatedon, lastupdatedby){
+        db("artists")
+        .where({name: name})
+        .returning("name")
+        .then(data => {
+            // ako je prazan array
+            if(!data.length){
+                db.insert(
+                    {
+                        name: name,
+                        image: image,
+                        alt: alt,
+                        genre: genre,
+                        city: city,
+                        web: web,
+                        submittedby: submittedby,
+                        submittedon: submittedon,
+                        lastupdatedon: lastupdatedon,
+                        lastupdatedby: lastupdatedby,
+                    }
+                )
+                .into("artists")
+                .then(() => {
+                    db("artists").select("*")
+                    .then(data => res.json({
+                        data: data,
+                        message: "new artist successfully added"
+                    }))
+                    .catch(err => res.json({
+                        data: err,
+                        message: "adding new artist failed"
+                    }))
+                })
+            }
+            else{
+                res.json({
+                    data: "",
+                    message: "an artist with this name already exists"
+                });
+            }
         })
-        if(!sameArtist){
-            artists.push(req.body);
-            res.json(artists);
-        }
-        else{
-            res.json("Izvođač pod tim imenom već postoji. Provjerite radi li se o istom izvođaču ili promijenite ime.")
-        }
     }
-    else{
-        res.json("Incomplete artist data");
+    else {
+        res.status(500).json({
+            data: "",
+            message: "Please fill all fields"
+        })
     }
+
+
 })
 
 // "/editartist"PUT - receive edited artist and return response
 app.put("/updateartist", (req, res) => {
-    const {id, name, image, alt, genre, city, web, submittedBy, submittedOn, lastUpdatedOn, lastUpdatedBy} = req.body;
+    // treba provdjeriti je li sve ispunjeno
+    console.log(req.body);
+    const {id, name, image, alt, genre, city, web, submittedby, submittedon, lastupdatedon, lastupdatedby} = req.body;
+    if(id, name, image, alt, genre, city, web, submittedby, submittedon, lastupdatedon, lastupdatedby){
 
-    if(id && name && image && alt && genre && city && web && submittedBy && submittedOn && lastUpdatedOn && lastUpdatedBy){
-        let artistForUpdate = false;
-        artists.forEach((artist, index) => {
-            if(artist.id === id){
-                artistForUpdate = true;
-                artists[index] = req.body;
-                return
+        db("artists")
+        .where({id: id})
+        .update(
+            {                        
+                name: name,
+                image: image,
+                alt: alt,
+                genre: genre,
+                city: city,
+                web: web,
+                lastupdatedon: lastupdatedon,
+                lastupdatedby: lastupdatedby,
             }
-            // ovdje bi trebao ici neki else u slucaju da se ipak ne pronade izvodac za update
+        )
+        .returning("*")
+        .then(artist => {
+
+            db("artists").select("*")
+            .then(artists => res.json({
+                data: {currentArtist: artist[0], artists: artists},
+                message: "the artist was successfully updated"
+            }))
+            .catch(err => res.json({
+                data: err,
+                message: "data retrieval failed"
+            }))
+
         })
-        if(artistForUpdate){
-            // res.json({data: req.body, message: "Ažuriranje uspješno"});
-            res.json(artists);
-        }
-        else{
-            res.json("Greška");
-        }
+        .catch(err => {
+            res.json(
+                {
+                    data: err,
+                    message: "there was an error with updating"
+                }
+            )
+        })
     }
     else{
-        res.json({data: {}, message: "Incomplete artist information"});
+        res.json(
+            {
+                data: "",
+                message: "no such artist was found"
+            }
+        )
     }
 })
 
