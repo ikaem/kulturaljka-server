2657c1a4c1b03baf478512de26e60f3b3e9f107a
Karlo Marinovic <36795522+ikaem@users.noreply.github.com>
2019-12-19 12:37:20 +0100

    setting up heroku db creds with knex

--- server.js
+++ server.js
@@ -3,12 +3,17 @@ const database = require("./tempDatabase");
 const bcrypt = require("bcryptjs");
 const cors = require("cors");
 const knex = require("knex")({
+        // client: "pg",
+        // connection: {
+        //     host: '127.0.0.1',
+        //     user: "postgres",
+        //     password: "anVvPRpp",
+        //     database: 'kulturaljka'
+        // }
         client: "pg",
         connection: {
-            host: '127.0.0.1',
-            user: "postgres",
-            password: "anVvPRpp",
-            database: 'kulturaljka'
+            connectionString: env.process.DATABASE_URL,
+            ssl: true
         }
     })
 
